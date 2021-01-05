63f1bb80db2490d1bbc6cec1cceada805afc2417
Stephen Wright <wrightsah@icloud.com>
2021-01-05 11:08:16 -0500

    created pseudocode for script

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+// Define Save button
+// use image from fontawesome
+
+// Dynamically create the time blocks
+
+// 1. Define a new time block. 
+
+// 2. Add the block for each time. 
+
+// 3. 
+
+
+// If time is future, set class to .future
+
+// If time is present, set class to .present
+
+// If time is past, set class to .past
+
