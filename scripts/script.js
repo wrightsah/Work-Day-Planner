a8ceb350b1c99e49c94b9ca37fe2aec8489025cd
Stephen Wright <wrightsah@icloud.com>
2021-01-07 19:07:53 -0500

    updated containiers to take up correct width

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,18 @@
 // Define Save button
 
-var saveBtn = 
 // use image from fontawesome
 
 
 // Time blocks need an input field.
 // Save (Append) text to time block when save button is pressed. 
 
+// Define current date
 
+// Define current time
 
+var currentDate = moment().calendar();
+
+console.log(currentDate);
 
 // If time is future, set class to .future
 
